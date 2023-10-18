23d5677497c4fb2d999ac9d0723f0e9093cb40d1
Cristhian Rojas <103226786+CristhianJR4@users.noreply.github.com>
2023-10-18 02:50:36 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,13 +2,6 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
-
     def do_POST(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
